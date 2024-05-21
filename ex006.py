3591b29417c6a0a807f0c3bfac75846b368dd924
Ronni <ronnivirgula@gmail.com>
2024-05-21 07:50:18 -0300

    Genérico

---
+++ ex006.py
@@ -0,0 +1,7 @@
+num = int(input('Digite um número: '))
+
+print('Dobro desse número {}'.format(num*2))
+print('Triplo desse número {}'.format(num*3))
+print('Raiz quadrada desse número {}'.format(num**(1/2)))
+# Outro método raiz quadrada
+# print('Raiz quadrada desse número {}'.format(pow(num, (1/2))))
