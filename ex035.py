7f64336471bec33c1d2658df25a8c758bf4d4c4a
Ronni <ronnivirgula@gmail.com>
2024-05-21 08:02:14 -0300

    Genérico

---
+++ ex035.py
@@ -0,0 +1,8 @@
+segmento1 = float(input('Digite o segmento 1: '))
+segmento2 = float(input('Digite o segmento 2: '))
+segmento3 = float(input('Digite o segmento 3: '))
+
+if segmento1 < segmento2 + segmento3 and segmento2 < segmento2 + segmento3 and segmento3 < segmento2 + segmento1:
+    print('Um triângulo pode ser feito.')
+else:
+    print('Um triângulo não pode ser feito.')
